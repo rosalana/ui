b182319f47a46789882936c8ee8cf89f4e4e092d
Michael Bany <mobany526@gmail.com>
2025-09-02 20:17:56 +0200

    start s async fetchovani

--- src/composables/types.ts
+++ src/composables/types.ts
@@ -1,1 +1,7 @@
 export type CreateModelOptions<T> = Record<string, Promise<T> | Promise<T>[]>;
+
+export type UseFetchOptions = {
+    method?: "GET" | "POST" | "PUT" | "DELETE" | 'get' | 'post' | 'put' | 'delete';
+    headers?: Record<string, string>;
+    body?: any;
+}
---
+++ src/composables/useFetch.ts
@@ -0,0 +1,48 @@
+import { UseFetchOptions } from './types'
+import axios from 'axios'
+
+export async function useFetch(url: string, options: UseFetchOptions = {}) {
+  const http = instance
+  const method = (options.method ?? 'GET').toLowerCase() as 'get' | 'post' | 'put' | 'delete'
+
+  const config = {
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...(options.headers ?? {}),
+    },
+    withCredentials: true,
+  }
+
+  try {
+    const response = await http.request({
+      url,
+      method,
+      data: options.body ?? undefined,
+      ...config,
+    })
+
+    console.log('🎁 Axios/Inertia response payload:', response.data)
+
+    return response.data
+  } catch (error: any) {
+    // Axios chyby mají response v error.response
+    if (error.response) {
+      throw new Error(`HTTP error! status: ${error.response.status}`)
+    }
+
+    throw error
+  }
+}
+
+const instance = axios.create({
+  baseURL: '/',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    Accept: 'application/json',
+  },
+  withCredentials: true,
+});
+
+
