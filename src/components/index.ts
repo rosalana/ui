e8c611edc5ea64941a9993507b96785e80a32cb2
Michael Baný <michael.bany@punktero.com>
2025-10-20 13:10:54 +0200

    refactor: move all components to Ui folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,98 +1,98 @@
 // UI
-export { default as UiButton } from "./ui/button/Button.vue";
-export { default as UiIcon } from "./ui/icon/Icon.vue";
+export { default as UiButton } from "./Ui/Button/Button.vue";
+export { default as UiIcon } from "./Ui/Icon/Icon.vue";
 
 // Blocks
 
 // Dialog
-export { default as UiDialog } from "./Dialog/Dialog.vue";
-export { default as UiDialogRoot } from "./Dialog/DialogRoot.vue";
-export { default as UiDialogTrigger } from "./Dialog/DialogTrigger.vue";
-export { default as UiDialogContent } from "./Dialog/DialogContent.vue";
-export { default as UiDialogOverlay } from "./Dialog/DialogOverlay.vue";
-export { default as UiDialogTitle } from "./Dialog/DialogTitle.vue";
-export { default as UiDialogDescription } from "./Dialog/DialogDescription.vue";
-export { default as UiDialogClose } from "./Dialog/DialogClose.vue";
-export { default as UiDialogPortal } from "./Dialog/DialogPortal.vue";
+export { default as UiDialog } from "./Ui/Dialog/Dialog.vue";
+export { default as UiDialogRoot } from "./Ui/Dialog/DialogRoot.vue";
+export { default as UiDialogTrigger } from "./Ui/Dialog/DialogTrigger.vue";
+export { default as UiDialogContent } from "./Ui/Dialog/DialogContent.vue";
+export { default as UiDialogOverlay } from "./Ui/Dialog/DialogOverlay.vue";
+export { default as UiDialogTitle } from "./Ui/Dialog/DialogTitle.vue";
+export { default as UiDialogDescription } from "./Ui/Dialog/DialogDescription.vue";
+export { default as UiDialogClose } from "./Ui/Dialog/DialogClose.vue";
+export { default as UiDialogPortal } from "./Ui/Dialog/DialogPortal.vue";
 
 // Popover
-export { default as UiPopover } from "./Popover/Popover.vue";
-export { default as UiPopoverRoot } from "./Popover/PopoverRoot.vue";
-export { default as UiPopoverTrigger } from "./Popover/PopoverTrigger.vue";
-export { default as UiPopoverContent } from "./Popover/PopoverContent.vue";
-export { default as UiPopoverAnchor } from "./Popover/PopoverAnchor.vue";
-export { default as UiPopoverPortal } from "./Popover/PopoverPortal.vue";
-export { default as UiPopoverClose } from "./Popover/PopoverClose.vue";
-export { default as UiPopoverArrow } from "./Popover/PopoverArrow.vue";
+export { default as UiPopover } from "./Ui/Popover/Popover.vue";
+export { default as UiPopoverRoot } from "./Ui/Popover/PopoverRoot.vue";
+export { default as UiPopoverTrigger } from "./Ui/Popover/PopoverTrigger.vue";
+export { default as UiPopoverContent } from "./Ui/Popover/PopoverContent.vue";
+export { default as UiPopoverAnchor } from "./Ui/Popover/PopoverAnchor.vue";
+export { default as UiPopoverPortal } from "./Ui/Popover/PopoverPortal.vue";
+export { default as UiPopoverClose } from "./Ui/Popover/PopoverClose.vue";
+export { default as UiPopoverArrow } from "./Ui/Popover/PopoverArrow.vue";
 
 // Tooltip
-export { default as UiTooltip } from "./Tooltip/Tooltip.vue";
-export { default as UiTooltipRoot } from "./Tooltip/TooltipRoot.vue";
-export { default as UiTooltipTrigger } from "./Tooltip/TooltipTrigger.vue";
-export { default as UiTooltipContent } from "./Tooltip/TooltipContent.vue";
-export { default as UiTooltipProvider } from "./Tooltip/TooltipProvider.vue";
-export { default as UiTooltipPortal } from "./Tooltip/TooltipPortal.vue";
-export { default as UiTooltipArrow } from "./Tooltip/TooltipArrow.vue";
+export { default as UiTooltip } from "./Ui/Tooltip/Tooltip.vue";
+export { default as UiTooltipRoot } from "./Ui/Tooltip/TooltipRoot.vue";
+export { default as UiTooltipTrigger } from "./Ui/Tooltip/TooltipTrigger.vue";
+export { default as UiTooltipContent } from "./Ui/Tooltip/TooltipContent.vue";
+export { default as UiTooltipProvider } from "./Ui/Tooltip/TooltipProvider.vue";
+export { default as UiTooltipPortal } from "./Ui/Tooltip/TooltipPortal.vue";
+export { default as UiTooltipArrow } from "./Ui/Tooltip/TooltipArrow.vue";
 
 // Dropdown Menu
-export { default as UiDropdownMenu } from "./DropdownMenu/DropdownMenu.vue";
-export { default as UiDropdownMenuRoot } from "./DropdownMenu/DropdownMenuRoot.vue";
-export { default as UiDropdownMenuTrigger } from "./DropdownMenu/DropdownMenuTrigger.vue";
-export { default as UiDropdownMenuContent } from "./DropdownMenu/DropdownMenuContent.vue";
-export { default as UiDropdownMenuItem } from "./DropdownMenu/DropdownMenuItem.vue";
-export { default as UiDropdownMenuLabel } from "./DropdownMenu/DropdownMenuLabel.vue";
-export { default as UiDropdownMenuSeparator } from "./DropdownMenu/DropdownMenuSeparator.vue";
-export { default as UiDropdownMenuGroup } from "./DropdownMenu/DropdownMenuGroup.vue";
-export { default as UiDropdownMenuCheckboxItem } from "./DropdownMenu/DropdownMenuCheckboxItem.vue";
-export { default as UiDropdownMenuRadioGroup } from "./DropdownMenu/DropdownMenuRadioGroup.vue";
-export { default as UiDropdownMenuRadioItem } from "./DropdownMenu/DropdownMenuRadioItem.vue";
-export { default as UiDropdownMenuItemIndicator } from "./DropdownMenu/DropdownMenuItemIndicator.vue";
-export { default as UiDropdownMenuSub } from "./DropdownMenu/DropdownMenuSub.vue";
-export { default as UiDropdownMenuSubTrigger } from "./DropdownMenu/DropdownMenuSubTrigger.vue";
-export { default as UiDropdownMenuSubContent } from "./DropdownMenu/DropdownMenuSubContent.vue";
-export { default as UiDropdownMenuArrow } from "./DropdownMenu/DropdownMenuArrow.vue";
-export { default as UiDropdownMenuPortal } from "./DropdownMenu/DropdownMenuPortal.vue";
+export { default as UiDropdownMenu } from "./Ui/DropdownMenu/DropdownMenu.vue";
+export { default as UiDropdownMenuRoot } from "./Ui/DropdownMenu/DropdownMenuRoot.vue";
+export { default as UiDropdownMenuTrigger } from "./Ui/DropdownMenu/DropdownMenuTrigger.vue";
+export { default as UiDropdownMenuContent } from "./Ui/DropdownMenu/DropdownMenuContent.vue";
+export { default as UiDropdownMenuItem } from "./Ui/DropdownMenu/DropdownMenuItem.vue";
+export { default as UiDropdownMenuLabel } from "./Ui/DropdownMenu/DropdownMenuLabel.vue";
+export { default as UiDropdownMenuSeparator } from "./Ui/DropdownMenu/DropdownMenuSeparator.vue";
+export { default as UiDropdownMenuGroup } from "./Ui/DropdownMenu/DropdownMenuGroup.vue";
+export { default as UiDropdownMenuCheckboxItem } from "./Ui/DropdownMenu/DropdownMenuCheckboxItem.vue";
+export { default as UiDropdownMenuRadioGroup } from "./Ui/DropdownMenu/DropdownMenuRadioGroup.vue";
+export { default as UiDropdownMenuRadioItem } from "./Ui/DropdownMenu/DropdownMenuRadioItem.vue";
+export { default as UiDropdownMenuItemIndicator } from "./Ui/DropdownMenu/DropdownMenuItemIndicator.vue";
+export { default as UiDropdownMenuSub } from "./Ui/DropdownMenu/DropdownMenuSub.vue";
+export { default as UiDropdownMenuSubTrigger } from "./Ui/DropdownMenu/DropdownMenuSubTrigger.vue";
+export { default as UiDropdownMenuSubContent } from "./Ui/DropdownMenu/DropdownMenuSubContent.vue";
+export { default as UiDropdownMenuArrow } from "./Ui/DropdownMenu/DropdownMenuArrow.vue";
+export { default as UiDropdownMenuPortal } from "./Ui/DropdownMenu/DropdownMenuPortal.vue";
 
 // Tabs
-export { default as UiTabs } from "./Tabs/Tabs.vue";
-export { default as UiTabsRoot } from "./Tabs/TabsRoot.vue";
-export { default as UiTabsList } from "./Tabs/TabsList.vue";
-export { default as UiTabsTrigger } from "./Tabs/TabsTrigger.vue";
-export { default as UiTabsContent } from "./Tabs/TabsContent.vue";
-export { default as UiTabsIndicator } from "./Tabs/TabsIndicator.vue";
+export { default as UiTabs } from "./Ui/Tabs/Tabs.vue";
+export { default as UiTabsRoot } from "./Ui/Tabs/TabsRoot.vue";
+export { default as UiTabsList } from "./Ui/Tabs/TabsList.vue";
+export { default as UiTabsTrigger } from "./Ui/Tabs/TabsTrigger.vue";
+export { default as UiTabsContent } from "./Ui/Tabs/TabsContent.vue";
+export { default as UiTabsIndicator } from "./Ui/Tabs/TabsIndicator.vue";
 
 // Switch
-export { default as UiSwitch } from "./Switch/Switch.vue";
-export { default as UiSwitchRoot } from "./Switch/SwitchRoot.vue";
-export { default as UiSwitchThumb } from "./Switch/SwitchThumb.vue";
+export { default as UiSwitch } from "./Ui/Switch/Switch.vue";
+export { default as UiSwitchRoot } from "./Ui/Switch/SwitchRoot.vue";
+export { default as UiSwitchThumb } from "./Ui/Switch/SwitchThumb.vue";
 
 // Checkbox
-export { default as UiCheckbox } from "./Checkbox/Checkbox.vue";
-export { default as UiCheckboxIndicator } from "./Checkbox/CheckboxIndicator.vue";
-export { default as UiCheckboxGroup } from "./Checkbox/CheckboxGroup.vue";
+export { default as UiCheckbox } from "./Ui/Checkbox/Checkbox.vue";
+export { default as UiCheckboxIndicator } from "./Ui/Checkbox/CheckboxIndicator.vue";
+export { default as UiCheckboxGroup } from "./Ui/Checkbox/CheckboxGroup.vue";
 
 // Radio Group
-export { default as UiRadioGroup } from "./RadioGroup/RadioGroup.vue";
-export { default as UiRadioGroupRoot } from "./RadioGroup/RadioGroupRoot.vue";
-export { default as UiRadioGroupItem } from "./RadioGroup/RadioGroupItem.vue";
-export { default as UiRadioGroupIndicator } from "./RadioGroup/RadioGroupIndicator.vue";
+export { default as UiRadioGroup } from "./Ui/RadioGroup/RadioGroup.vue";
+export { default as UiRadioGroupRoot } from "./Ui/RadioGroup/RadioGroupRoot.vue";
+export { default as UiRadioGroupItem } from "./Ui/RadioGroup/RadioGroupItem.vue";
+export { default as UiRadioGroupIndicator } from "./Ui/RadioGroup/RadioGroupIndicator.vue";
 
 // Separator
-export { default as UiSeparator } from "./Separator/Separator.vue";
+export { default as UiSeparator } from "./Ui/Separator/Separator.vue";
 
 // Select
-export { default as UiSelect } from "./Select/Select.vue";
-export { default as UiSelectRoot } from "./Select/SelectRoot.vue";
-export { default as UiSelectTrigger } from "./Select/SelectTrigger.vue";
-export { default as UiSelectContent } from "./Select/SelectContent.vue";
-export { default as UiSelectItem } from "./Select/SelectItem.vue";
-export { default as UiSelectLabel } from "./Select/SelectLabel.vue";
-export { default as UiSelectGroup } from "./Select/SelectGroup.vue";
-export { default as UiSelectSeparator } from "./Select/SelectSeparator.vue";
-export { default as UiSelectValue } from "./Select/SelectValue.vue";
-export { default as UiSelectPortal } from "./Select/SelectPortal.vue";
-export { default as UiSelectArrow } from "./Select/SelectArrow.vue";
-export { default as UiSelectViewport } from "./Select/SelectViewport.vue";
+export { default as UiSelect } from "./Ui/Select/Select.vue";
+export { default as UiSelectRoot } from "./Ui/Select/SelectRoot.vue";
+export { default as UiSelectTrigger } from "./Ui/Select/SelectTrigger.vue";
+export { default as UiSelectContent } from "./Ui/Select/SelectContent.vue";
+export { default as UiSelectItem } from "./Ui/Select/SelectItem.vue";
+export { default as UiSelectLabel } from "./Ui/Select/SelectLabel.vue";
+export { default as UiSelectGroup } from "./Ui/Select/SelectGroup.vue";
+export { default as UiSelectSeparator } from "./Ui/Select/SelectSeparator.vue";
+export { default as UiSelectValue } from "./Ui/Select/SelectValue.vue";
+export { default as UiSelectPortal } from "./Ui/Select/SelectPortal.vue";
+export { default as UiSelectArrow } from "./Ui/Select/SelectArrow.vue";
+export { default as UiSelectViewport } from "./Ui/Select/SelectViewport.vue";
 
 // Input
-export { default as UiInput } from "./Input/Input.vue";
+export { default as UiInput } from "./Ui/Input/Input.vue";
