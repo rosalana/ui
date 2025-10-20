1c36d4983f5e4dca6c8ec4dccb06657b4658ac80
Michael Baný <michael.bany@punktero.com>
2025-10-20 13:07:00 +0200

    feat: add comprehensive UI components including Dialog, Popover, Tooltip, Dropdown Menu, Tabs, Switch, Checkbox, Radio Group, Separator, Select, and Input

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,96 @@ export { default as UiButton } from "./ui/button/Button.vue";
 export { default as UiIcon } from "./ui/icon/Icon.vue";
 
 // Blocks
+
+// Dialog
+export { default as UiDialog } from "./Dialog/Dialog.vue";
+export { default as UiDialogRoot } from "./Dialog/DialogRoot.vue";
+export { default as UiDialogTrigger } from "./Dialog/DialogTrigger.vue";
+export { default as UiDialogContent } from "./Dialog/DialogContent.vue";
+export { default as UiDialogOverlay } from "./Dialog/DialogOverlay.vue";
+export { default as UiDialogTitle } from "./Dialog/DialogTitle.vue";
+export { default as UiDialogDescription } from "./Dialog/DialogDescription.vue";
+export { default as UiDialogClose } from "./Dialog/DialogClose.vue";
+export { default as UiDialogPortal } from "./Dialog/DialogPortal.vue";
+
+// Popover
+export { default as UiPopover } from "./Popover/Popover.vue";
+export { default as UiPopoverRoot } from "./Popover/PopoverRoot.vue";
+export { default as UiPopoverTrigger } from "./Popover/PopoverTrigger.vue";
+export { default as UiPopoverContent } from "./Popover/PopoverContent.vue";
+export { default as UiPopoverAnchor } from "./Popover/PopoverAnchor.vue";
+export { default as UiPopoverPortal } from "./Popover/PopoverPortal.vue";
+export { default as UiPopoverClose } from "./Popover/PopoverClose.vue";
+export { default as UiPopoverArrow } from "./Popover/PopoverArrow.vue";
+
+// Tooltip
+export { default as UiTooltip } from "./Tooltip/Tooltip.vue";
+export { default as UiTooltipRoot } from "./Tooltip/TooltipRoot.vue";
+export { default as UiTooltipTrigger } from "./Tooltip/TooltipTrigger.vue";
+export { default as UiTooltipContent } from "./Tooltip/TooltipContent.vue";
+export { default as UiTooltipProvider } from "./Tooltip/TooltipProvider.vue";
+export { default as UiTooltipPortal } from "./Tooltip/TooltipPortal.vue";
+export { default as UiTooltipArrow } from "./Tooltip/TooltipArrow.vue";
+
+// Dropdown Menu
+export { default as UiDropdownMenu } from "./DropdownMenu/DropdownMenu.vue";
+export { default as UiDropdownMenuRoot } from "./DropdownMenu/DropdownMenuRoot.vue";
+export { default as UiDropdownMenuTrigger } from "./DropdownMenu/DropdownMenuTrigger.vue";
+export { default as UiDropdownMenuContent } from "./DropdownMenu/DropdownMenuContent.vue";
+export { default as UiDropdownMenuItem } from "./DropdownMenu/DropdownMenuItem.vue";
+export { default as UiDropdownMenuLabel } from "./DropdownMenu/DropdownMenuLabel.vue";
+export { default as UiDropdownMenuSeparator } from "./DropdownMenu/DropdownMenuSeparator.vue";
+export { default as UiDropdownMenuGroup } from "./DropdownMenu/DropdownMenuGroup.vue";
+export { default as UiDropdownMenuCheckboxItem } from "./DropdownMenu/DropdownMenuCheckboxItem.vue";
+export { default as UiDropdownMenuRadioGroup } from "./DropdownMenu/DropdownMenuRadioGroup.vue";
+export { default as UiDropdownMenuRadioItem } from "./DropdownMenu/DropdownMenuRadioItem.vue";
+export { default as UiDropdownMenuItemIndicator } from "./DropdownMenu/DropdownMenuItemIndicator.vue";
+export { default as UiDropdownMenuSub } from "./DropdownMenu/DropdownMenuSub.vue";
+export { default as UiDropdownMenuSubTrigger } from "./DropdownMenu/DropdownMenuSubTrigger.vue";
+export { default as UiDropdownMenuSubContent } from "./DropdownMenu/DropdownMenuSubContent.vue";
+export { default as UiDropdownMenuArrow } from "./DropdownMenu/DropdownMenuArrow.vue";
+export { default as UiDropdownMenuPortal } from "./DropdownMenu/DropdownMenuPortal.vue";
+
+// Tabs
+export { default as UiTabs } from "./Tabs/Tabs.vue";
+export { default as UiTabsRoot } from "./Tabs/TabsRoot.vue";
+export { default as UiTabsList } from "./Tabs/TabsList.vue";
+export { default as UiTabsTrigger } from "./Tabs/TabsTrigger.vue";
+export { default as UiTabsContent } from "./Tabs/TabsContent.vue";
+export { default as UiTabsIndicator } from "./Tabs/TabsIndicator.vue";
+
+// Switch
+export { default as UiSwitch } from "./Switch/Switch.vue";
+export { default as UiSwitchRoot } from "./Switch/SwitchRoot.vue";
+export { default as UiSwitchThumb } from "./Switch/SwitchThumb.vue";
+
+// Checkbox
+export { default as UiCheckbox } from "./Checkbox/Checkbox.vue";
+export { default as UiCheckboxIndicator } from "./Checkbox/CheckboxIndicator.vue";
+export { default as UiCheckboxGroup } from "./Checkbox/CheckboxGroup.vue";
+
+// Radio Group
+export { default as UiRadioGroup } from "./RadioGroup/RadioGroup.vue";
+export { default as UiRadioGroupRoot } from "./RadioGroup/RadioGroupRoot.vue";
+export { default as UiRadioGroupItem } from "./RadioGroup/RadioGroupItem.vue";
+export { default as UiRadioGroupIndicator } from "./RadioGroup/RadioGroupIndicator.vue";
+
+// Separator
+export { default as UiSeparator } from "./Separator/Separator.vue";
+
+// Select
+export { default as UiSelect } from "./Select/Select.vue";
+export { default as UiSelectRoot } from "./Select/SelectRoot.vue";
+export { default as UiSelectTrigger } from "./Select/SelectTrigger.vue";
+export { default as UiSelectContent } from "./Select/SelectContent.vue";
+export { default as UiSelectItem } from "./Select/SelectItem.vue";
+export { default as UiSelectLabel } from "./Select/SelectLabel.vue";
+export { default as UiSelectGroup } from "./Select/SelectGroup.vue";
+export { default as UiSelectSeparator } from "./Select/SelectSeparator.vue";
+export { default as UiSelectValue } from "./Select/SelectValue.vue";
+export { default as UiSelectPortal } from "./Select/SelectPortal.vue";
+export { default as UiSelectArrow } from "./Select/SelectArrow.vue";
+export { default as UiSelectViewport } from "./Select/SelectViewport.vue";
+
+// Input
+export { default as UiInput } from "./Input/Input.vue";
