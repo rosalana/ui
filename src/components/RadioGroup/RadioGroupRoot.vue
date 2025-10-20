ab68deec648565a2edb0ea2ba819620e02cc8c1d
Michael Baný <michael.bany@punktero.com>
2025-10-20 13:05:26 +0200

    feat: add RadioGroup components with customizable styles and props

---
+++ src/components/RadioGroup/RadioGroupRoot.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { RadioGroupRootProps as RekaRadioGroupRootProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { RadioGroupRoot as RekaRadioGroupRoot } from "reka-ui"
+import { tv } from "tailwind-variants"
+
+defineOptions({ name: "RadioGroupRoot" })
+
+const root = tv({ base: "flex flex-col gap-2 outline-none" })
+
+interface Props extends RekaRadioGroupRootProps {
+  class?: HTMLAttributes["class"]
+}
+
+const props = defineProps<Props>()
+
+export type RadioGroupRootProps = Props
+</script>
+
+<template>
+  <RekaRadioGroupRoot v-bind="{ ...props, class: [root(), props.class] }">
+    <slot />
+  </RekaRadioGroupRoot>
+</template>
+
+
