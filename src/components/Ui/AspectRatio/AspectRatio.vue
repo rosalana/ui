1b0b185db102bdc30f0ec8d50d0469a22d039062
Michael Bany <mobany526@gmail.com>
2026-01-24 18:03:41 +0100

    refactor: improve code formatting in AspectRatio component

--- src/components/Ui/AspectRatio/AspectRatio.vue
+++ src/components/Ui/AspectRatio/AspectRatio.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import type { AspectRatioProps } from "reka-ui"
-import { AspectRatio, useForwardProps } from "reka-ui"
+import type { AspectRatioProps } from "reka-ui";
+import { AspectRatio, useForwardProps } from "reka-ui";
 
-const props = defineProps<AspectRatioProps>()
-const forwarded = useForwardProps(props)
+const props = defineProps<AspectRatioProps>();
+const forwarded = useForwardProps(props);
 </script>
 
 <template>
