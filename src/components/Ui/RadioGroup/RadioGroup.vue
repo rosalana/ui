1a72f1a923e163e555e87c84d3f60d00d3f7979b
Michael Bany <mobany526@gmail.com>
2026-01-24 15:32:34 +0100

    feat: add RadioGroup and RadioGroupItem components with customizable props and styling

---
+++ src/components/Ui/RadioGroup/RadioGroup.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { RadioGroupRootEmits, RadioGroupRootProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { RadioGroupRoot, useForwardPropsEmits } from "reka-ui"
+import { tv } from "tailwind-variants"
+
+const radioGroup = tv({
+  base: "grid gap-2",
+})
+
+interface Props extends RadioGroupRootProps {
+  class?: HTMLAttributes["class"]
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<RadioGroupRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emit)
+</script>
+
+<template>
+  <RadioGroupRoot
+    data-slot="radio-group"
+    v-bind="forwarded"
+    :class="[radioGroup(), props.class]"
+  >
+    <slot />
+  </RadioGroupRoot>
+</template>
