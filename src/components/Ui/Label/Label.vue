01343da96272a6cf130e0601d93a16d15fe54425
Michael Bany <mobany526@gmail.com>
2026-01-24 15:30:52 +0100

    feat: add Label component with customizable props and styling

---
+++ src/components/Ui/Label/Label.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { LabelProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { Label } from "reka-ui"
+import { tv } from "tailwind-variants"
+
+const label = tv({
+  base: "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+})
+
+interface Props extends LabelProps {
+  class?: HTMLAttributes["class"]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <Label
+    data-slot="label"
+    v-bind="props"
+    :class="[label(), props.class]"
+  >
+    <slot />
+  </Label>
+</template>
