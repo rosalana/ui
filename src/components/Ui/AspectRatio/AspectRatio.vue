91b16089dea99ab6abab01383580858b8cf4abc2
Michael Bany <mobany526@gmail.com>
2026-01-24 15:26:05 +0100

    feat: add AspectRatio component

---
+++ src/components/Ui/AspectRatio/AspectRatio.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { AspectRatioProps } from "reka-ui"
+import { AspectRatio, useForwardProps } from "reka-ui"
+
+const props = defineProps<AspectRatioProps>()
+const forwarded = useForwardProps(props)
+</script>
+
+<template>
+  <AspectRatio data-slot="aspect-ratio" v-bind="forwarded">
+    <slot />
+  </AspectRatio>
+</template>
