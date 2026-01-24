8100bba1d2b0761090c3a6bf79ee69c9cb2c4949
Michael Bany <mobany526@gmail.com>
2026-01-24 15:25:37 +0100

    feat: add Alert component and its subcomponents (AlertDescription, AlertTitle)

---
+++ src/components/Ui/Alert/AlertDescription.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { Primitive } from "reka-ui"
+import { tv } from "tailwind-variants"
+
+const alertDescription = tv({
+  base: "text-sm [&_p]:leading-relaxed",
+})
+
+interface Props extends PrimitiveProps {
+  class?: HTMLAttributes["class"]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <Primitive
+    data-slot="alert-description"
+    :as="as ?? 'div'"
+    :as-child="asChild"
+    :class="[alertDescription(), props.class]"
+  >
+    <slot />
+  </Primitive>
+</template>
---
+++ src/components/Ui/Alert/AlertTitle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { PrimitiveProps } from "reka-ui"
+import type { HTMLAttributes } from "vue"
+import { Primitive } from "reka-ui"
+import { tv } from "tailwind-variants"
+
+const alertTitle = tv({
+  base: "mb-1 font-medium leading-none tracking-tight",
+})
+
+interface Props extends PrimitiveProps {
+  class?: HTMLAttributes["class"]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <Primitive
+    data-slot="alert-title"
+    :as="as ?? 'h5'"
+    :as-child="asChild"
+    :class="[alertTitle(), props.class]"
+  >
+    <slot />
+  </Primitive>
+</template>
