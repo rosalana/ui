6735f575294338361d2490522c540b88ebbeafc7
Michael Bany <mobany526@gmail.com>
2026-01-24 15:37:46 +0100

    feat: reorganize component exports in index.ts with categorized sections for better readability

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,265 @@
-// UI
+// UI Components
+
+// Accordion
+export { default as UiAccordion } from "./Ui/Accordion/Accordion.vue";
+export { default as UiAccordionItem } from "./Ui/Accordion/AccordionItem.vue";
+export { default as UiAccordionTrigger } from "./Ui/Accordion/AccordionTrigger.vue";
+export { default as UiAccordionContent } from "./Ui/Accordion/AccordionContent.vue";
+
+// Alert
+export { default as UiAlert } from "./Ui/Alert/Alert.vue";
+export { default as UiAlertTitle } from "./Ui/Alert/AlertTitle.vue";
+export { default as UiAlertDescription } from "./Ui/Alert/AlertDescription.vue";
+
+// AlertDialog
+export { default as UiAlertDialog } from "./Ui/AlertDialog/AlertDialog.vue";
+export { default as UiAlertDialogTrigger } from "./Ui/AlertDialog/AlertDialogTrigger.vue";
+export { default as UiAlertDialogContent } from "./Ui/AlertDialog/AlertDialogContent.vue";
+export { default as UiAlertDialogHeader } from "./Ui/AlertDialog/AlertDialogHeader.vue";
+export { default as UiAlertDialogFooter } from "./Ui/AlertDialog/AlertDialogFooter.vue";
+export { default as UiAlertDialogTitle } from "./Ui/AlertDialog/AlertDialogTitle.vue";
+export { default as UiAlertDialogDescription } from "./Ui/AlertDialog/AlertDialogDescription.vue";
+export { default as UiAlertDialogAction } from "./Ui/AlertDialog/AlertDialogAction.vue";
+export { default as UiAlertDialogCancel } from "./Ui/AlertDialog/AlertDialogCancel.vue";
+
+// AspectRatio
+export { default as UiAspectRatio } from "./Ui/AspectRatio/AspectRatio.vue";
+
+// Avatar
+export { default as UiAvatar } from "./Ui/Avatar/Avatar.vue";
+export { default as UiAvatarImage } from "./Ui/Avatar/AvatarImage.vue";
+export { default as UiAvatarFallback } from "./Ui/Avatar/AvatarFallback.vue";
+
+// Badge
+export { default as UiBadge } from "./Ui/Badge/Badge.vue";
+
+// Breadcrumb
+export { default as UiBreadcrumb } from "./Ui/Breadcrumb/Breadcrumb.vue";
+export { default as UiBreadcrumbList } from "./Ui/Breadcrumb/BreadcrumbList.vue";
+export { default as UiBreadcrumbItem } from "./Ui/Breadcrumb/BreadcrumbItem.vue";
+export { default as UiBreadcrumbLink } from "./Ui/Breadcrumb/BreadcrumbLink.vue";
+export { default as UiBreadcrumbPage } from "./Ui/Breadcrumb/BreadcrumbPage.vue";
+export { default as UiBreadcrumbSeparator } from "./Ui/Breadcrumb/BreadcrumbSeparator.vue";
+export { default as UiBreadcrumbEllipsis } from "./Ui/Breadcrumb/BreadcrumbEllipsis.vue";
+
+// Button
 export { default as UiButton } from "./Ui/Button/Button.vue";
+
+// Calendar
+export { default as UiCalendar } from "./Ui/Calendar/Calendar.vue";
+
+// Card
+export { default as UiCard } from "./Ui/Card/Card.vue";
+export { default as UiCardHeader } from "./Ui/Card/CardHeader.vue";
+export { default as UiCardTitle } from "./Ui/Card/CardTitle.vue";
+export { default as UiCardDescription } from "./Ui/Card/CardDescription.vue";
+export { default as UiCardContent } from "./Ui/Card/CardContent.vue";
+export { default as UiCardFooter } from "./Ui/Card/CardFooter.vue";
+
+// Checkbox
+export { default as UiCheckbox } from "./Ui/Checkbox/Checkbox.vue";
+
+// Collapsible
+export { default as UiCollapsible } from "./Ui/Collapsible/Collapsible.vue";
+export { default as UiCollapsibleTrigger } from "./Ui/Collapsible/CollapsibleTrigger.vue";
+export { default as UiCollapsibleContent } from "./Ui/Collapsible/CollapsibleContent.vue";
+
+// Command
+export { default as UiCommand } from "./Ui/Command/Command.vue";
+export { default as UiCommandInput } from "./Ui/Command/CommandInput.vue";
+export { default as UiCommandList } from "./Ui/Command/CommandList.vue";
+export { default as UiCommandEmpty } from "./Ui/Command/CommandEmpty.vue";
+export { default as UiCommandGroup } from "./Ui/Command/CommandGroup.vue";
+export { default as UiCommandItem } from "./Ui/Command/CommandItem.vue";
+export { default as UiCommandSeparator } from "./Ui/Command/CommandSeparator.vue";
+export { default as UiCommandShortcut } from "./Ui/Command/CommandShortcut.vue";
+export { default as UiCommandDialog } from "./Ui/Command/CommandDialog.vue";
+
+// ContextMenu
+export { default as UiContextMenu } from "./Ui/ContextMenu/ContextMenu.vue";
+export { default as UiContextMenuTrigger } from "./Ui/ContextMenu/ContextMenuTrigger.vue";
+export { default as UiContextMenuContent } from "./Ui/ContextMenu/ContextMenuContent.vue";
+export { default as UiContextMenuItem } from "./Ui/ContextMenu/ContextMenuItem.vue";
+export { default as UiContextMenuCheckboxItem } from "./Ui/ContextMenu/ContextMenuCheckboxItem.vue";
+export { default as UiContextMenuRadioGroup } from "./Ui/ContextMenu/ContextMenuRadioGroup.vue";
+export { default as UiContextMenuRadioItem } from "./Ui/ContextMenu/ContextMenuRadioItem.vue";
+export { default as UiContextMenuLabel } from "./Ui/ContextMenu/ContextMenuLabel.vue";
+export { default as UiContextMenuSeparator } from "./Ui/ContextMenu/ContextMenuSeparator.vue";
+export { default as UiContextMenuShortcut } from "./Ui/ContextMenu/ContextMenuShortcut.vue";
+export { default as UiContextMenuGroup } from "./Ui/ContextMenu/ContextMenuGroup.vue";
+export { default as UiContextMenuSub } from "./Ui/ContextMenu/ContextMenuSub.vue";
+export { default as UiContextMenuSubTrigger } from "./Ui/ContextMenu/ContextMenuSubTrigger.vue";
+export { default as UiContextMenuSubContent } from "./Ui/ContextMenu/ContextMenuSubContent.vue";
+
+// Dialog
+export { default as UiDialog } from "./Ui/Dialog/Dialog.vue";
+export { default as UiDialogTrigger } from "./Ui/Dialog/DialogTrigger.vue";
+export { default as UiDialogPortal } from "./Ui/Dialog/DialogPortal.vue";
+export { default as UiDialogOverlay } from "./Ui/Dialog/DialogOverlay.vue";
+export { default as UiDialogContent } from "./Ui/Dialog/DialogContent.vue";
+export { default as UiDialogHeader } from "./Ui/Dialog/DialogHeader.vue";
+export { default as UiDialogFooter } from "./Ui/Dialog/DialogFooter.vue";
+export { default as UiDialogTitle } from "./Ui/Dialog/DialogTitle.vue";
+export { default as UiDialogDescription } from "./Ui/Dialog/DialogDescription.vue";
+export { default as UiDialogClose } from "./Ui/Dialog/DialogClose.vue";
+
+// DropdownMenu
+export { default as UiDropdownMenu } from "./Ui/DropdownMenu/DropdownMenu.vue";
+export { default as UiDropdownMenuTrigger } from "./Ui/DropdownMenu/DropdownMenuTrigger.vue";
+export { default as UiDropdownMenuContent } from "./Ui/DropdownMenu/DropdownMenuContent.vue";
+export { default as UiDropdownMenuItem } from "./Ui/DropdownMenu/DropdownMenuItem.vue";
+export { default as UiDropdownMenuCheckboxItem } from "./Ui/DropdownMenu/DropdownMenuCheckboxItem.vue";
+export { default as UiDropdownMenuRadioGroup } from "./Ui/DropdownMenu/DropdownMenuRadioGroup.vue";
+export { default as UiDropdownMenuRadioItem } from "./Ui/DropdownMenu/DropdownMenuRadioItem.vue";
+export { default as UiDropdownMenuLabel } from "./Ui/DropdownMenu/DropdownMenuLabel.vue";
+export { default as UiDropdownMenuSeparator } from "./Ui/DropdownMenu/DropdownMenuSeparator.vue";
+export { default as UiDropdownMenuShortcut } from "./Ui/DropdownMenu/DropdownMenuShortcut.vue";
+export { default as UiDropdownMenuGroup } from "./Ui/DropdownMenu/DropdownMenuGroup.vue";
+export { default as UiDropdownMenuSub } from "./Ui/DropdownMenu/DropdownMenuSub.vue";
+export { default as UiDropdownMenuSubTrigger } from "./Ui/DropdownMenu/DropdownMenuSubTrigger.vue";
+export { default as UiDropdownMenuSubContent } from "./Ui/DropdownMenu/DropdownMenuSubContent.vue";
+
+// HoverCard
+export { default as UiHoverCard } from "./Ui/HoverCard/HoverCard.vue";
+export { default as UiHoverCardTrigger } from "./Ui/HoverCard/HoverCardTrigger.vue";
+export { default as UiHoverCardContent } from "./Ui/HoverCard/HoverCardContent.vue";
+
+// Icon
 export { default as UiIcon } from "./Ui/Icon/Icon.vue";
+
+// Input
+export { default as UiInput } from "./Ui/Input/Input.vue";
+
+// Label
+export { default as UiLabel } from "./Ui/Label/Label.vue";
+
+// Menubar
+export { default as UiMenubar } from "./Ui/Menubar/Menubar.vue";
+export { default as UiMenubarMenu } from "./Ui/Menubar/MenubarMenu.vue";
+export { default as UiMenubarTrigger } from "./Ui/Menubar/MenubarTrigger.vue";
+export { default as UiMenubarContent } from "./Ui/Menubar/MenubarContent.vue";
+export { default as UiMenubarItem } from "./Ui/Menubar/MenubarItem.vue";
+export { default as UiMenubarCheckboxItem } from "./Ui/Menubar/MenubarCheckboxItem.vue";
+export { default as UiMenubarRadioGroup } from "./Ui/Menubar/MenubarRadioGroup.vue";
+export { default as UiMenubarRadioItem } from "./Ui/Menubar/MenubarRadioItem.vue";
+export { default as UiMenubarLabel } from "./Ui/Menubar/MenubarLabel.vue";
+export { default as UiMenubarSeparator } from "./Ui/Menubar/MenubarSeparator.vue";
+export { default as UiMenubarShortcut } from "./Ui/Menubar/MenubarShortcut.vue";
+export { default as UiMenubarGroup } from "./Ui/Menubar/MenubarGroup.vue";
+export { default as UiMenubarSub } from "./Ui/Menubar/MenubarSub.vue";
+export { default as UiMenubarSubTrigger } from "./Ui/Menubar/MenubarSubTrigger.vue";
+export { default as UiMenubarSubContent } from "./Ui/Menubar/MenubarSubContent.vue";
+
+// NavigationMenu
+export { default as UiNavigationMenu } from "./Ui/NavigationMenu/NavigationMenu.vue";
+export { default as UiNavigationMenuList } from "./Ui/NavigationMenu/NavigationMenuList.vue";
+export { default as UiNavigationMenuItem } from "./Ui/NavigationMenu/NavigationMenuItem.vue";
+export { default as UiNavigationMenuTrigger } from "./Ui/NavigationMenu/NavigationMenuTrigger.vue";
+export { default as UiNavigationMenuContent } from "./Ui/NavigationMenu/NavigationMenuContent.vue";
+export { default as UiNavigationMenuLink } from "./Ui/NavigationMenu/NavigationMenuLink.vue";
+export { default as UiNavigationMenuIndicator } from "./Ui/NavigationMenu/NavigationMenuIndicator.vue";
+
+// Pagination
+export { default as UiPagination } from "./Ui/Pagination/Pagination.vue";
+export { default as UiPaginationList } from "./Ui/Pagination/PaginationList.vue";
+export { default as UiPaginationListItem } from "./Ui/Pagination/PaginationListItem.vue";
+export { default as UiPaginationEllipsis } from "./Ui/Pagination/PaginationEllipsis.vue";
+export { default as UiPaginationFirst } from "./Ui/Pagination/PaginationFirst.vue";
+export { default as UiPaginationPrev } from "./Ui/Pagination/PaginationPrev.vue";
+export { default as UiPaginationNext } from "./Ui/Pagination/PaginationNext.vue";
+export { default as UiPaginationLast } from "./Ui/Pagination/PaginationLast.vue";
+
+// PinInput
+export { default as UiPinInput } from "./Ui/PinInput/PinInput.vue";
+export { default as UiPinInputInput } from "./Ui/PinInput/PinInputInput.vue";
+
+// Popover
+export { default as UiPopover } from "./Ui/Popover/Popover.vue";
+export { default as UiPopoverTrigger } from "./Ui/Popover/PopoverTrigger.vue";
+export { default as UiPopoverContent } from "./Ui/Popover/PopoverContent.vue";
+export { default as UiPopoverAnchor } from "./Ui/Popover/PopoverAnchor.vue";
+export { default as UiPopoverClose } from "./Ui/Popover/PopoverClose.vue";
+
+// Progress
+export { default as UiProgress } from "./Ui/Progress/Progress.vue";
+
+// RadioGroup
+export { default as UiRadioGroup } from "./Ui/RadioGroup/RadioGroup.vue";
+export { default as UiRadioGroupItem } from "./Ui/RadioGroup/RadioGroupItem.vue";
+
+// Resizable
+export { default as UiResizablePanelGroup } from "./Ui/Resizable/ResizablePanelGroup.vue";
+export { default as UiResizablePanel } from "./Ui/Resizable/ResizablePanel.vue";
+export { default as UiResizableHandle } from "./Ui/Resizable/ResizableHandle.vue";
+
+// ScrollArea
+export { default as UiScrollArea } from "./Ui/ScrollArea/ScrollArea.vue";
+export { default as UiScrollBar } from "./Ui/ScrollArea/ScrollBar.vue";
+
+// Select
+export { default as UiSelect } from "./Ui/Select/Select.vue";
+export { default as UiSelectTrigger } from "./Ui/Select/SelectTrigger.vue";
+export { default as UiSelectValue } from "./Ui/Select/SelectValue.vue";
+export { default as UiSelectContent } from "./Ui/Select/SelectContent.vue";
+export { default as UiSelectItem } from "./Ui/Select/SelectItem.vue";
+export { default as UiSelectGroup } from "./Ui/Select/SelectGroup.vue";
+export { default as UiSelectLabel } from "./Ui/Select/SelectLabel.vue";
+export { default as UiSelectSeparator } from "./Ui/Select/SelectSeparator.vue";
+export { default as UiSelectScrollUpButton } from "./Ui/Select/SelectScrollUpButton.vue";
+export { default as UiSelectScrollDownButton } from "./Ui/Select/SelectScrollDownButton.vue";
+
+// Separator
+export { default as UiSeparator } from "./Ui/Separator/Separator.vue";
+
+// Sheet
+export { default as UiSheet } from "./Ui/Sheet/Sheet.vue";
+export { default as UiSheetTrigger } from "./Ui/Sheet/SheetTrigger.vue";
+export { default as UiSheetClose } from "./Ui/Sheet/SheetClose.vue";
+export { default as UiSheetContent } from "./Ui/Sheet/SheetContent.vue";
+export { default as UiSheetHeader } from "./Ui/Sheet/SheetHeader.vue";
+export { default as UiSheetFooter } from "./Ui/Sheet/SheetFooter.vue";
+export { default as UiSheetTitle } from "./Ui/Sheet/SheetTitle.vue";
+export { default as UiSheetDescription } from "./Ui/Sheet/SheetDescription.vue";
+
+// Skeleton
+export { default as UiSkeleton } from "./Ui/Skeleton/Skeleton.vue";
+
+// Slider
+export { default as UiSlider } from "./Ui/Slider/Slider.vue";
+
+// Switch
+export { default as UiSwitch } from "./Ui/Switch/Switch.vue";
+
+// Table
+export { default as UiTable } from "./Ui/Table/Table.vue";
+export { default as UiTableHeader } from "./Ui/Table/TableHeader.vue";
+export { default as UiTableBody } from "./Ui/Table/TableBody.vue";
+export { default as UiTableFooter } from "./Ui/Table/TableFooter.vue";
+export { default as UiTableRow } from "./Ui/Table/TableRow.vue";
+export { default as UiTableHead } from "./Ui/Table/TableHead.vue";
+export { default as UiTableCell } from "./Ui/Table/TableCell.vue";
+export { default as UiTableCaption } from "./Ui/Table/TableCaption.vue";
+
+// Tabs
+export { default as UiTabs } from "./Ui/Tabs/Tabs.vue";
+export { default as UiTabsList } from "./Ui/Tabs/TabsList.vue";
+export { default as UiTabsTrigger } from "./Ui/Tabs/TabsTrigger.vue";
+export { default as UiTabsContent } from "./Ui/Tabs/TabsContent.vue";
+
+// Textarea
+export { default as UiTextarea } from "./Ui/Textarea/Textarea.vue";
+
+// Toggle
+export { default as UiToggle } from "./Ui/Toggle/Toggle.vue";
+
+// ToggleGroup
+export { default as UiToggleGroup } from "./Ui/ToggleGroup/ToggleGroup.vue";
+export { default as UiToggleGroupItem } from "./Ui/ToggleGroup/ToggleGroupItem.vue";
+
+// Tooltip
+export { default as UiTooltip } from "./Ui/Tooltip/Tooltip.vue";
+export { default as UiTooltipTrigger } from "./Ui/Tooltip/TooltipTrigger.vue";
+export { default as UiTooltipContent } from "./Ui/Tooltip/TooltipContent.vue";
+export { default as UiTooltipProvider } from "./Ui/Tooltip/TooltipProvider.vue";
 
 // Blocks
