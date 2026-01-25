2bdf0379e9568042beb1e192b852eded8759fc05
Michael Bany <mobany526@gmail.com>
2026-01-26 00:07:30 +0100

    feat: enhance Switch component with improved styling and motion effects

--- src/components/Ui/Switch/Switch.vue
+++ src/components/Ui/Switch/Switch.vue
@@ -3,13 +3,30 @@ import type { SwitchRootEmits, SwitchRootProps } from "reka-ui";
 import type { HTMLAttributes } from "vue";
 import { SwitchRoot, SwitchThumb, useForwardPropsEmits } from "reka-ui";
 import { tv } from "tailwind-variants";
+import { motion } from "motion-v";
 
 const switchRoot = tv({
-  base: "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-xs transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+  base: [
+    "peer cursor-pointer inline-flex items-center h-5 w-9 shrink-0 rounded-full border border-border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary",
+    "shadow-[0_2px_8px_-3px,0_4px_20px_-4px] shadow-black/15",
+    "hover:shadow-primary hover:border-primary/30",
+    "data-[state=checked]:shadow-primary data-[state=checked]:border-primary",
+    "hover:data-[state=checked]:shadow-primary",
+    "transition-all duration-150",
+    "active:scale-[0.90]",
+  ],
 });
 
 const switchThumb = tv({
-  base: "pointer-events-none block size-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+  base: [
+    "pointer-events-none block size-4 rounded-full",
+    "bg-background ring-0 shadow-lg",
+    "flex items-center justify-center",
+    "transition-all duration-150 ease-in-out",
+    "data-[state=unchecked]:bg-border data-[state=checked]:text-primary data-[state=unchecked]:text-background",
+    "data-[state=unchecked]:[&>div]:bg-background data-[state=checked]:[&>div]:bg-primary",
+    "data-[state=checked]:[&>div]:h-2 data-[state=checked]:[&>div]:w-1",
+  ],
 });
 
 interface Props extends SwitchRootProps {
@@ -20,6 +37,8 @@ const props = defineProps<Props>();
 const emit = defineEmits<SwitchRootEmits>();
 
 const forwarded = useForwardPropsEmits(props, emit);
+
+const checked = defineModel<boolean>();
 </script>
 
 <template>
@@ -27,7 +46,27 @@ const forwarded = useForwardPropsEmits(props, emit);
     data-slot="switch"
     v-bind="forwarded"
     :class="[switchRoot({ class: props.class })]"
+    @update:model-value="(v) => (checked = v)"
+    :model-value="checked"
   >
-    <SwitchThumb :class="switchThumb()" />
+    <SwitchThumb :class="switchThumb()" as-child>
+      <motion.div
+        :animate="
+          checked
+            ? { transform: 'translateX(16px)' }
+            : { transform: 'translateX(2px)' }
+        "
+        :transition="{
+          duration: 0.15,
+          stiffness: 500,
+          damping: 25,
+          type: 'spring',
+        }"
+      >
+        <div
+          class="size-1.5 shrink-0 rounded-full transition-colors duration-150 ease-in-out"
+        />
+      </motion.div>
+    </SwitchThumb>
   </SwitchRoot>
 </template>
