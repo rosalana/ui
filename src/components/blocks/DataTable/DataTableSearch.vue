7da3c797554f5bfc510a11c2ee830212a3c34d25
Michael Bany <mobany526@gmail.com>
2026-01-25 19:14:57 +0100

    style: simplify DataTableSearch component by removing clear button functionality

--- src/components/blocks/DataTable/DataTableSearch.vue
+++ src/components/blocks/DataTable/DataTableSearch.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts" generic="T">
-import { UiButton, UiIcon, UiInput } from "../../index";
+import { UiInput } from "../../index";
 import { useTable } from "../../../composables";
-import { Transition } from "vue";
 
 const props = defineProps<{
   table: ReturnType<typeof useTable<T>>;
@@ -14,39 +13,8 @@ const props = defineProps<{
       placeholder="Search..."
       name="search"
       autocomplete="off"
+      icon="lucide:search"
+      show-clear
     />
-
-    <div class="absolute top-1/2 right-3 -translate-y-1/2 flex items-center">
-      <Transition
-        name="fade"
-        mode="out-in"
-        enter-active-class="transition-opacity duration-200"
-        leave-active-class="transition-opacity duration-200"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-      >
-        <UiButton
-          v-if="table.search"
-          key="clear"
-          variant="link"
-          size="icon-sm"
-          class="size-4 p-0 text-destructive"
-          @click="table.search = ''"
-        >
-          <UiIcon name="lucide:x" class="size-3.5" />
-        </UiButton>
-        <UiButton
-          v-else
-          key="not-clear"
-          variant="link"
-          size="icon-sm"
-          class="size-4 p-0 pointer-events-none"
-        >
-          <UiIcon key="search" name="lucide:search" class="size-3.5" />
-        </UiButton>
-      </Transition>
-    </div>
   </div>
 </template>
