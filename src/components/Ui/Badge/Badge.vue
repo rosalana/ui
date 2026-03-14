65a55e0cfdb8b7c71d7935b2458c1c6b6194fa4b
Michael Bany <mobany526@gmail.com>
2026-03-15 00:30:18 +0100

    style: enhance destructive, success, warning, and info badge styles for better dark mode support

--- src/components/Ui/Badge/Badge.vue
+++ src/components/Ui/Badge/Badge.vue
@@ -21,19 +21,19 @@ const badge = tv({
         // "shadow-[0_1px_8px_-2px,0_1px_15px_-2px] shadow-secondary/40",
       ],
       destructive: [
-        "border-destructive-200 bg-destructive-50 text-destructive hover:bg-destructive-100",
+        "border-destructive-200 bg-destructive-50 text-destructive text-destructive hover:bg-destructive-100 dark:bg-destructive-950 dark:hover:bg-destructive-900 dark:border-destructive-700",
         // "shadow-[0_1px_3px_-2px,0_1px_15px_-2px] shadow-destructive/30",
       ],
       success: [
-        "border-success-200 bg-success-50 text-success hover:bg-success-200",
+        "border-success-200 bg-success-50 text-success hover:bg-success-200 dark:bg-success-950 dark:hover:bg-success-800 dark:border-success-700",
         // "shadow-[0_1px_3px_-2px,0_1px_15px_-2px] shadow-success/30",
       ],
       warning: [
-        "border-warning-200 bg-warning-50 text-warning hover:bg-warning-200",
+        "border-warning-200 bg-warning-50 text-warning hover:bg-warning-200 dark:bg-warning-950 dark:hover:bg-warning-900 dark:border-warning-700",
         // "shadow-[0_1px_3px_-2px,0_1px_15px_-2px] shadow-warning/30",
       ],
       info: [
-        "border-info-200 bg-info-50 text-info hover:bg-info-200",
+        "border-info-200 bg-info-50 text-info hover:bg-info-200 dark:bg-info-950 dark:hover:bg-info-900 dark:border-info-700",
         // "shadow-[0_1px_3px_-2px,0_1px_15px_-2px] shadow-info/30",
       ],
       outline: "text-foreground hover:bg-muted border-border bg-background",
